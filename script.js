110d86c5eb41a4076736a2eca8a20dbd57894e4e
rafiazharr17 <rafipilo17@gmail.com>
2024-04-07 02:08:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("gambar").addEventListener("click", function () {
+  alert("Ini gambar yip-yip bobok");
+});
+
+document.getElementById("video").addEventListener("click", function () {
+  alert("Ini video yip-yip baru dibeli");
+});
+
+document.getElementById("audio").addEventListener("click", function () {
+  alert("Ini audio jj yip-yip");
+});
